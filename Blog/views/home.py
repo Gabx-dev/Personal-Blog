415379be33c38837bfc07d9f85afc84159d01077
Gabx-dev <58875500+Gabx-dev@users.noreply.github.com>
2022-08-19 15:12:21 -0300

    Updated home to render new home template

--- Blog/views/home.py
+++ Blog/views/home.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from Blog.models import Post
 
 
-def homePageView(req):
-  return HttpResponse(content='Hello World!')
+def homePageView(request):
+  posts = Post.objects.filter().order_by('created_on')
+  template = loader.get_template('Blog/index.html')
+
+  context = {
+    'post_list': posts
+  }
+
+  return HttpResponse(template.render(context, request))
