4569a74f722d98a8a91286c134236b75f380ed2e
Gabx-dev <58875500+Gabx-dev@users.noreply.github.com>
2022-08-19 15:09:59 -0300

    Added tests for post view and updated tests for home

--- Blog/tests/test_views.py
+++ Blog/tests/test_views.py
@@ -1,12 +1,28 @@
 import pytest
 
 from django.test import client
+from django.contrib.auth.models import User
+from django.urls import reverse
 
+from Blog.models import Post
+
+@pytest.mark.django_db()
 def test_home(client):
   url = ''
-  response_content = b'Hello World!'
-
   res = client.get(url)
-
   assert res.status_code == 200
-  assert res.content == response_content
+
+@pytest.mark.django_db
+def test_post(client):
+  user = User.objects.create_user('testUser', 'user@test.io', '102030')
+  post = Post.objects.create(
+    title='Test post',
+    slug='test-post',
+    author=user,
+    content='This is a test post'
+  )
+
+  url = reverse('post_detail', args=[post.slug])
+  print(url)
+  res = client.get(url)
+  assert res.status_code == 200
