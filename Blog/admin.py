af8a812be4091ce3ad267e57f5d88e6a4c8fd422
Gabx-dev <58875500+Gabx-dev@users.noreply.github.com>
2022-08-18 21:45:55 -0300

    registered post model in the Django admin site

--- Blog/admin.py
+++ Blog/admin.py
@@ -2,3 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = [
+    'title',
+    'author',
+    'created_on',
+    'published_on',
+    'status'
+  ]
+
+  list_filter = ['status', 'created_on']
+  prepopulated_fields = {'slug': ('title',)}
